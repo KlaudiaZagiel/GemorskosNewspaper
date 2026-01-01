d32927fb0e6d95542dd499fcd555105f22eecfee
Klaudia <klaudia.zagiel@student.nhlstenden.com>
2026-01-01 15:48:23 +0100

    Adjusting login page a bit. - Klaudia Zagiel 1.1.2026

--- logintest2/index.php
+++ logintest2/index.php
@@ -1,58 +1,59 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Login</title>
-<link rel="stylesheet" href="style/style.css?v=2">
+  <title>Login</title>
+  <link rel="stylesheet" href="style/style.css?v=2">
 </head>
 
 <body>
 
-<!-- top navbar -->
-<header>
-        <div class="navbar">
-            <h2>Gemorskos</h2>
-            <ul>
-                <li><a href="home.html" class="home">Home</a></li>
-                <li><a href="index.php" class="login">Login</a></li>
-            </ul>
-        </div>
-    </header>
-
-<!-- header section -->
-<div class="headbox">
-  <h1>Login</h1>
-  <p>Home > Login</p>
-</div>
-
-<!-- login card -->
-<div class="login-card">
-
-  <!-- message area -->
-  <p class="msg">
-  <?php
-  if(isset($_GET["msg"]))
-  {
-      echo $_GET["msg"];
-  }
-  ?>
-  </p>
-
-  <form action="login.php" method="post">
-
-    <label>Username or Email</label>
-    <input type="text" name="eml">
-
-    <label>Password</label>
-    <input type="password" name="pass1">
-
-    <div class="remember">
-      <input type="checkbox"> Remember Me
+  <!-- top navbar -->
+  <header>
+    <div class="navbar">
+      <h2>Gemorskos</h2>
+      <ul>
+        <li><a href="home.html" class="home">Home</a></li>
+        <li><a href="index.php" class="login">Login</a></li>
+      </ul>
     </div>
+  </header>
 
-    <button type="submit" name="btn1">Log in</button>
+  <!-- header section -->
+  <div class="headbox">
+    <h1>Login</h1>
+    <p>Welcome at Gemorskos Newspaper</p>
+  </div>
 
-  </form>
-</div>
+  <!-- login card -->
+  <div class="login-card">
+
+    <!-- message area -->
+    <p class="msg">
+      <?php
+      if (isset($_GET["msg"])) {
+        echo $_GET["msg"];
+      }
+      ?>
+    </p>
+
+    <form action="login.php" method="post">
+
+      <label>Username or Email</label>
+      <input type="text" name="eml">
+
+      <label>Password</label>
+      <input type="password" name="pass1">
+
+      <!-- <div class="remember">
+        <input type="checkbox"> Remember Me
+      </div> -->
+
+      <button type="submit" name="btn1">Log in</button>
+
+    </form>
+  </div>
 
 </body>
+
 </html>
