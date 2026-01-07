8ffa84afdf5ccff868cdf85cb1c2ed0e7f98465e
Aynur <aynur.tozluyurt@student.nhlstenden.com>
2026-01-07 15:35:45 +0100

    Creating the database, added db.php, adjusted the login.php but I'm still not done

---
+++ logintest2/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$dbname = "gemorskos_newspaper";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed");
+}
---
+++ logintest2/loginvalidation.php
@@ -0,0 +1,38 @@
+<?php
+
+// this was created by Muhammad Umar; I'm keeping it here just incase
+// I created a database so I'm changing things a little!!
+
+if(isset($_POST["btn1"]))
+{
+    $a1 = $_POST["eml"];
+    $b2 = $_POST["pass1"];
+
+    if($a1 == "" || $b2 == "")
+    {
+        header("Location: index.php?msg=Enter all required information.");
+    }
+    else
+    {
+        if(!filter_var($a1, FILTER_VALIDATE_EMAIL))
+        {
+            header("Location: index.php?msg=Email is not valid.");
+        }
+        else
+        {
+            if(strlen($b2) < 8 || !preg_match("/[A-Z]/",$b2))
+            {
+                header("Location: index.php?msg=Your password is too weak.");
+            }
+            else
+            {
+                // in case of success → show message then redirect
+                echo "Logged in thank you";
+
+                header("refresh:2; url=homepage.html");
+            }
+        }
+    }
+}
+
+?>
