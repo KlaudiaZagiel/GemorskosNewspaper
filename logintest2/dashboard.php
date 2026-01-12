28796ee33d5d789fbd36230fcec4735ebe7aac57
Aynur <aynur.tozluyurt@student.nhlstenden.com>
2026-01-12 15:19:31 +0100

    updated access control

--- logintest2/dashboard.php
+++ logintest2/dashboard.php
@@ -26,6 +26,19 @@ if($_SESSION["role"] === "admin") {
     <h1>Welcome, <?php echo htmlspecialchars($_SESSION["full_name"] ?? $_SESSION ["username"]) ?></h1>
     <p>Your role is <?php echo htmlspecialchars($_SESSION["role"]) ?></p>
 
-    <!-- The rest will come later. -->
+    <?php if($_SESSION["role"] === "editor"): ?>
+        <p>You have editor access. You can manage articles.</p>
+
+    <?php elseif($_SESSION["role"] === "journalist"): ?>
+            <p>You have journalist access. You can write and submit articles.</p>
+
+    <?php elseif($_SESSION["role"] === "webdesigner"): ?>
+            <p>You have webdesigner access. You can design webpages.</p>
+
+    <?php else: ?>
+        <p>Your role does not have specific access rights.</p>
+    <?php endif; ?>
+
+    <a href="logout.php">Logout</a> <!-- yet to come-->
 </body>
 </html>
